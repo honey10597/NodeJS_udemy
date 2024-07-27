87850ee4fec3c418a715453fed3bd522a3237931
Honey Kumar <honeykumar@codebrewinnovations.com>
2024-07-27 15:07:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require("http")
+const fs = require("fs")
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    const method = req.method
+
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter title</title></head>')
+        res.write(
+            '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">SEND</button></form></body>'
+        )
+        res.write('</html>')
+        return res.end()
+    }
+
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader
+            })
+        })
+    }
+
+    res.setHeader("Content-Type", "text/html")
+    res.write('<html>')
+    res.write('<head><title>Enter title</title></head>')
+    res.write('<body>hello from node js server</body>')
+    res.write('</html>')
+    return res.end()
+
+})
+
+server.listen(3000)
